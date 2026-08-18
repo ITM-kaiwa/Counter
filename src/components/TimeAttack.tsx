8e84e934c7fa6a417e2b348c1f16cb135af4322d
ITM-Group Web App <ohkawatakeyoshi@itmgroup.com.vn>
2026-08-18 13:27:01 +0700

    Fix unused import TypeScript error in TimeAttack.tsx

--- src/components/TimeAttack.tsx
+++ src/components/TimeAttack.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import { countersData } from '../data/counters';
-import { Volume2 } from 'lucide-react';
 import { playAudio } from '../utils/tts';
 
 const TimeAttack = () => {
